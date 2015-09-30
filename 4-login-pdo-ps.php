2535750855eb62873a7cea130d0c38e33b8a5ece
agustin de deluca <agustin_deluca@hotmail.com>
2015-09-29 22:38:54 -0300

    test conexion v1

---
+++ 4-login-pdo-ps.php
@@ -0,0 +1,39 @@
+<?php
+
+//conection:
+$db_host="localhost";
+$db_user="grupo_10";
+$db_pass="DCerbxcYhyEA9X4T";
+$db_base="grupo_10"; 
+
+try {
+	$cn = new PDO("mysql:dbname=$db_base;host=$db_host",$db_user,$db_pass);
+	
+	
+	$query = $cn->prepare("SELECT * FROM usuarios ");
+	$query->execute(array($_POST["email"],$_POST["pass"])); 
+
+	//display information:
+	echo "<br>Variables Recibidas:";
+	echo"<br>Uusuario: <strong>". $_POST["email"]."</strong>";
+	echo"<br>PASS: <strong>". $_POST["pass"]."</strong>";
+	
+	echo "<br> El query que hicimos fue: Ver log de mysql.....";
+
+	echo "<br> El n&uacute;mero de resultados fue " . $query->rowCount();
+	
+	echo "<br> <br>Objeto resultado ";
+	print_r($query);
+	
+	echo "<br> <br>Elementos resultado ";
+	
+	print_r($query->fetchAll());
+	
+	}
+catch(PDOException $e){
+	echo "ERROR". $e->getMessage();
+	}
+
+
+
+?>
--- pruebaPropel/build.php
+++ pruebaPropel/build.php
@@ -1,4 +1,4 @@
 
 <?php
-propel reverse "mysql:host=localhost;dbname=grupo_10;user=grupo_10;password=DCerbxcYhyEA9X4T"
+require propel reverse "mysql:host=https://sql.proyecto2015.linti.unlp.edu.ar;dbname=grupo_10;user=grupo_10;password=DCerbxcYhyEA9X4T"
 ?>
