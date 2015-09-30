a79a53c0e0ca2bf318d22a2384d1406554dd51aa
root <mauriciop_96@live.com>
2015-09-30 01:16:54 -0300

    ordenando 2

--- pruebaPropel/build.php
+++
@@ -1,5 +0,0 @@
-
-<?php
-require propel reverse "mysql:host=https://sql.proyecto2015.linti.unlp.edu.ar;dbname=grupo_10;user=grupo_10;password=DCerbxcYhyEA9X4T"
-?>
-<a href="../4-login-pdo-ps.php"></a>
