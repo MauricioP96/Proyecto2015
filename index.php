a40f2afc5f59a10a054697e9e8e2dd779810ae51
root <mauriciop_96@live.com>
2015-09-30 01:22:37 -0300

    conectando db

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 //Chekear si tiene sesion iniciada. If true redireccionar a backend
 
 
-require ("utilidadesphp/coneccionBD.php");
+require ("utilidadesphp/consultaConf.php");
 
 
 //Hacer consulta bd para datos de configuracion
