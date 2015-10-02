25c709e9f1c26d1a82f8e9e464383a739945be33
Agustin De Luca <agustin_deluca@hotmail.com>
2015-10-02 04:20:23 +0000

    .

--- ConfigAdmin.php
+++ ConfigAdmin.php
@@ -6,6 +6,7 @@ if(empty($_SESSION['nombreusuario'])){
    if (($_SESSION['rol']) == ("administracion")){
        require ('utilidadesphp/consultaConf.php');
        if (!empty($_POST['clave'])){
+           echo "jajaj"
            $query = $cn->prepare("UPDATE configuracion SET titulo = ?, descripcion = ?, mailContacto = ?, cantElem = ?, habilitada = ?, textoDeshab = ? WHERE 1");
            $query->execute(array($_POST['titulo'],$_POST['descripcion'],$_POST['mailContacto'],$_POST['cantElem'],$_POST['habilitada'],$_POST['textoDeshab'])); 
            header ("Location: ConfigAdmin.php");
@@ -17,6 +18,6 @@ if(empty($_SESSION['nombreusuario'])){
        
    }
    else {
-       header ("Location: backend.php");
+       header ("Location: index.php");
    }
 ?>
