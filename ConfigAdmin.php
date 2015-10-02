214331b3920df8da64e71ffac347f045baece10f
agustin de deluca <agustin_deluca@hotmail.com>
2015-10-02 00:13:41 -0300

    agregamos condiguracion admin

---
+++ ConfigAdmin.php
@@ -0,0 +1,24 @@
+<?php
+//if(empty($_SESSION['nombreusuario'])){
+ //   header ("Location: index.php");	
+//}
+//else{
+ //  if (($_SESSION['rol']) != ("administracion")){
+       require ('utilidadesphp/consultaConf.php');
+       if (!empty($_POST['clave'])){
+           $query = $cn->prepare("UPDATE configuracion SET titulo = ?, descripcion = ?, mailContacto = ?, cantElem = ?, habilitada = ?, textoDeshab = ? WHERE 1");
+           $query->execute(array($_POST['titulo'],$_POST['descripcion'],$_POST['mailContacto'],$_POST['cantElem'],$_POST['habilitada'],$_POST['textoDeshab'])); 
+           header ("Location: ConfigAdmin.php");
+       }
+       
+       require ('utilidadesphp/setearTwig.php');
+       $template = $twig->loadTemplate('ConfigAdmin.html');
+       $template -> display(array('datos' => $configuraciones['0']));
+       
+ //  }
+ //  else {
+ //      header ("Location: backend.php");
+ //  }
+   
+//}                
+?>
