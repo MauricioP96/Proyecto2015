e873d2f4ab64cbdb2ffbc325e2cef05615169d72
Juan Riglos <juanriglos@hotmail.com>
2015-10-02 22:28:09 -0300

    Alta de alumno, con validacion de dni

--- modelo/modelo_alumno.php
+++ modelo/modelo_alumno.php
@@ -1,10 +1,25 @@
 <?php
 
-function guardar_alumno($nombre){
-	require("coneccionBD.php");
-	$query = $cn->prepare("INSERT INTO Alumnos (nombre) VALUES (?)");
-	$query->execute(array($nombre)); 
-	
+function guardar_alumno($nombre,$apellido,$dni,$fecha_nacimiento,$sexo,$mail,$direccion,$fecha_ingreso,$fecha_egreso,$fecha_alta){
+	require("../modelo/coneccionBD.php");
+
+
+	$nuevo_alumno= $cn->prepare("SELECT numeroDoc FROM Alumnos WHERE numeroDoc=?");
+	$nuevo_alumno->execute(array($dni));
+	$rows=$nuevo_alumno->fetchAll();
+
+	if ($nuevo_alumno->rowCount() > 0) {
+		$fallo=true;
+	}
+	else{
+
+	$query = $cn->prepare("INSERT INTO Alumnos (nombre, apellido, numeroDoc, fechaNacimiento,
+	 sexo, mail, direccion, fechaIngreso, fechaEgreso, fechaAlta) VALUES (?,?,?,?,?,?,?,?,?,?)");
+	$aux=$query->execute(array($nombre,$apellido,$dni,$fecha_nacimiento,$sexo,$mail,$direccion,$fecha_ingreso,$fecha_egreso,$fecha_alta)); 
+	}
+	return $fallo;
 }
+
+	
 
 ?>
