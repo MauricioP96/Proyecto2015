0a9a2173abe2301163135e042947c7d05e13d7b6
mauriciop_96 <mauriciop_96@live.com>
2015-10-05 07:26:05 -0300

    modulo de modificacion y baja de cuotas

--- controlador/controlador_modificarAlumno.php
+++ controlador/controlador_modificarAlumno.php
@@ -5,8 +5,8 @@ session_start();
 if(empty($_SESSION['nombreusuario'])){						//Chekear si tiene sesion iniciada. If false redireccionar a index
 	header ("Location: index.php");
 	}								
-	elseif($_SESSION['rol']!='aministracion'){			//si el usuario no es administrador no darle permiso
-	///header ("Location: index.php");					
+	elseif($_SESSION['rol']!='aministracion' ){			//si el usuario no es administrador no darle permiso
+		header ("Location: ../controlador_login.php");					
 }
 
 if(!empty($_POST['idalumnoParaModificar'])){
---
+++ modelo/consultaCuotas.php
@@ -0,0 +1,34 @@
+<?php
+require ("../modelo/coneccionBD.php");
+require("../modelo/setearpagina.php");
+$query = $cn->prepare("SELECT count(*) as num FROM Cuotas WHERE Cuotas.eliminada=FALSE and NOT EXISTS(Select *
+                                                                               FROM Pagos
+                                                                               WHERE Cuotas.id=Pagos.idCuota)");
+$query->execute(); 
+$consultacant = $query->fetchAll();
+
+$cantidadalumnos=intval($consultacant[0]['num']);   //consulto la cantidad de tuplas totales sin paginar q debo mostrar
+
+//var_dump($consultacant);
+//var_dump($cantidadalumnos);
+//var_dump($pagina);
+
+$offset=(($pagina-1)*$configuraciones['0']['cantElem']);
+//var_dump($offset);
+$sss=intval($configuraciones['0']['cantElem']);
+$cantidadpaginas= intval(ceil($cantidadalumnos/$sss));  //calculo cuantas paginas debo mostrar
+//var_dump($cantidadpaginas);
+$query2=$cn->prepare("SELECT * FROM Cuotas WHERE eliminada=FALSE and NOT EXISTS(Select *
+                                                                               FROM Pagos
+                                                                               WHERE Cuotas.id=Pagos.idCuota) 
+               ORDER BY fechaAlta LIMIT :cantidad OFFSET :offset ");
+$query2->bindValue(':cantidad', $sss, PDO::PARAM_INT);
+$query2->bindValue(':offset', $offset, PDO::PARAM_INT);
+$query2->execute();
+//$error=$query2->errorInfo();
+//var_dump($error);
+
+$cuotas=$query2->fetchAll();
+//var_dump($alumnos);
+
+?>
