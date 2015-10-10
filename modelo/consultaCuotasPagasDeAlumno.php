8a8a913d2965d78abaae0810d9fae0eb7d5441dd
mauriciop_96 <mauriciop_96@live.com>
2015-10-10 19:27:05 -0300

    listado cuotas pagas e impagas v2

--- modelo/consultaCuotasPagasDeAlumno.php
+++ modelo/consultaCuotasPagasDeAlumno.php
@@ -1,7 +1,7 @@
 <?php
 
 require ("../modelo/coneccionBD.php");
-$query = $cn->prepare("SELECT Pagos.fecha, Cuotas.anio,Cuotas.mes,Cuotas.numero,Cuotas.monto,Cuotas.tipo,Cuotas.comisionCob,Meses.nombre
+$query = $cn->prepare("SELECT Pagos.fecha, Cuotas.anio,Cuotas.mes,Cuotas.numero,Cuotas.monto,Cuotas.tipo,Cuotas.comisionCob,Meses.nombre,Pagos.becado
 						FROM Pagos INNER JOIN Cuotas ON (Pagos.idAlumno=? AND Pagos.idCuota=Cuotas.id) INNER JOIN Meses ON (Cuotas.mes=Meses.idMes)
 						WHERE Cuotas.eliminada=FALSE
 						ORDER BY anio DESC, mes DESC");
