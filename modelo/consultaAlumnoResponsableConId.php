dadf0c2bbef482fda5e8f4a329723180e0a816ba
Juan Riglos <juanriglos@hotmail.com>
2015-10-14 01:52:24 -0300

    ahora si

--- modelo/consultaAlumnoResponsableConId.php
+++ modelo/consultaAlumnoResponsableConId.php
@@ -1,9 +1,10 @@
 <?php
 
 require ("../modelo/coneccionBD.php");
-$query = $cn->prepare("SELECT * FROM AlumnoResponsable  WHERE idAlumno=?");
+$query = $cn->prepare("SELECT idResponsable FROM AlumnoResponsable  WHERE idAlumno=?");
 $query->execute(array($idalu)); 
 $alumno_responsable = $query->fetchAll();
+var_dump($alumno_responsable);
 
 ?>
 
