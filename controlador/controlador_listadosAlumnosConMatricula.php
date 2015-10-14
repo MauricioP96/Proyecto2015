5a822e655666af0035aed338f0e03d364df76560
Juan Riglos <juanriglos@hotmail.com>
2015-10-14 11:43:37 -0300

    no toque ninguno pero hay q hacer mere...

--- controlador/controlador_listadosAlumnosConMatricula.php
+++ controlador/controlador_listadosAlumnosConMatricula.php
@@ -5,24 +5,8 @@ if(empty($_SESSION['nombreusuario'])){
     header ("Location: frontend_controller.php");	
 }
 require ('../modelo/consultaConf.php');
-require('../modelo/setearTwig.php');
-if ($_SESSION['rol'] == "consulta"){
-     require ('../modelo/consultaAlumnosPropios.php');
-      $template = $twig->loadTemplate('listadosAlumnosInformacionPropios.html');
-  $template->display(array('titulo' => $configuraciones['0']['titulo'],
-              'contacto' => $configuraciones['0']['mailContacto'],
-              'tipo' => $_SESSION['rol'],
-                          'alumnosConMatricula' => $alumnosConMatricula,
-                          'cantpaginas' => $cantidadpaginas,
-                          'paginaactual' => $pagina,
-                          'datospost' => $datosprepost
-              )); 
-
-
-}
-else{
-   
 require ('../modelo/consultaAlumnosConMatricula.php');
+require('../modelo/setearTwig.php');
   $template = $twig->loadTemplate('listadosAlumnosConMatricula.html');
   $template->display(array('titulo' => $configuraciones['0']['titulo'],
               'contacto' => $configuraciones['0']['mailContacto'],
@@ -32,7 +16,4 @@ require ('../modelo/consultaAlumnosConMatricula.php');
                           'paginaactual' => $pagina,
                           'datospost' => $datosprepost
               ));
-}
-
-
 ?>
