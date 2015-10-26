acf97c1ad589ebca74f182c96557bcc5c20c1fac
mauriciop_96 <mauriciop_96@live.com>
2015-10-26 04:44:51 -0300

    probando pago cuota 2

--- modelo/registrar_pago.php
+++ modelo/registrar_pago.php
@@ -4,12 +4,14 @@ require("../modelo/coneccionBD.php");
 	$pago= $cn->prepare("INSERT INTO Pagos (idAlumno,idCuota,fecha,fechaAlta,becado,fechaActualizado)
 							 VALUES (?,?,CURRENT_TIME,CURRENT_TIME,?,CURRENT_TIME)");
 	var_dump($pago);
-
+	var_dump($idcuotas);
 	foreach ($idcuotas as $idcuota){
-		//var_dump($idcuota[0]);
+		var_dump($idcuota[0]);
 		$aux=array($idalumno,$idcuota[0],$debobecar);
-		//var_dump($aux);
+		var_dump($aux);
 		$pago->execute($aux);
+		$error=$pago->errorInfo();
+		var_dump($error);
 	}
 	$agrego=true;
 	//var_dump($dni);
