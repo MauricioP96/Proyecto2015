ef093091551515ad91b3be805440e202e5a04a7a
mauriciop_96 <mauriciop_96@live.com>
2015-11-02 18:07:55 -0300

    arreglado setearTwig para MVC

--- modelo/setearTwig.php
+++ modelo/setearTwig.php
@@ -1,11 +1,11 @@
 <?php 
 
+function setearTwig(&$loader,&$twig){
+	require_once("../vendor/twig/twig/lib/Twig/Autoloader.php");
+	Twig_Autoloader::register();
 
-require_once("../vendor/twig/twig/lib/Twig/Autoloader.php");
-Twig_Autoloader::register();
-
-$templateDir="../vista";
-$loader = new Twig_Loader_Filesystem($templateDir);
-$twig = new Twig_Environment($loader);
-
+	$templateDir="../vista";
+	$loader = new Twig_Loader_Filesystem($templateDir);
+	$twig = new Twig_Environment($loader);
+}
 ?>
