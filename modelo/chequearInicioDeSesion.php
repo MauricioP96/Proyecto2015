9c86027844e822e1ff411271db59541c56d8a373
mauriciop_96 <mauriciop_96@live.com>
2015-11-02 18:52:46 -0300

    frontend pasadas a MVC

--- modelo/chequearInicioDeSesion.php
+++ modelo/chequearInicioDeSesion.php
@@ -1,26 +1,26 @@
 <?php
+function iniciar_sesion($cn,$usuario,$clave){
+    $query = $cn->prepare("SELECT * FROM Usuarios WHERE username=? and password=? and habilitado=TRUE");
+    $query->execute(array($_POST["usuario"],$_POST["clave"])); 
+    $rows = $query->fetchAll();
+    //print_r($rows);
 
-require ("coneccionBD.php");
-$query = $cn->prepare("SELECT * FROM Usuarios WHERE username=? and password=? and habilitado=TRUE");
-$query->execute(array($_POST["usuario"],$_POST["clave"])); 
-$rows = $query->fetchAll();
-//print_r($rows);
+    if($query->rowCount()==1){
+       session_destroy();
+       session_start();
+       foreach ($rows as $row) {
+       		 $_SESSION['nombreusuario'] = $row['username'];
+    		$_SESSION['rol'] = $row['rol'];
+    	//echo $_SESSION['nombreusuario'];
 
-if($query->rowCount()==1){
-   session_destroy();
-   session_start();
-   foreach ($rows as $row) {
-   		 $_SESSION['nombreusuario'] = $row['username'];
-		$_SESSION['rol'] = $row['rol'];
-	//echo $_SESSION['nombreusuario'];
-
-	//echo $_SESSION['rol'];
-	}
-  header ("Location: controlador_login.php");
-         
+    	//echo $_SESSION['rol'];
+    	}
+      $mostrofallo=false;
+             
+    }
+    else{
+       $mostrofallo=true;
+    }
+    return $mostrofallo;
 }
-else{
-   $mostrofallo=true;
-}
-
 ?>
