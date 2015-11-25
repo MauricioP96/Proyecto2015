a2ef9444b14253fa964e928111389cd7bfe2de23
agustin de deluca <agustin_deluca@hotmail.com>
2015-11-25 05:18:24 -0300

    181

--- modelo/consultaAlumnosPropiosEXPORTACION.php
+++ modelo/consultaAlumnosPropiosEXPORTACION.php
@@ -1,5 +1,5 @@
 <?php 
-function consultaAlumnosPropios($cn,&$datosprepost,$pagina,$tipodel,$nombreusuario,&$cantidadpaginas,$configuraciones){
+function consultaAlumnosPropiosEXPORTACION($cn,&$datosprepost,$pagina,$tipodel,$nombreusuario,&$cantidadpaginas,$configuraciones){
 if ((empty($tipodel))  || $tipodel == 1){
 $query = $cn->prepare("SELECT count(*) as num 
 	                    FROM Alumnos INNER JOIN Pagos ON (Alumnos.id=Pagos.idAlumno) 
@@ -16,15 +16,13 @@ $offset=(($pagina-1)*$configuraciones['0']['cantElem']);
 $sss=intval($configuraciones['0']['cantElem']);
 $cantidadpaginas= intval(ceil($cantidadalumnos/$sss)); 
 $query2=$cn->prepare("SELECT * 
-	                    FROM Alumnos INNER JOIN Pagos ON (Alumnos.id=Pagos.idAlumno) 
+	          
 	                                 INNER JOIN Cuotas ON (Pagos.idCuota=Cuotas.id) 
 	                                 INNER JOIN AlumnoResponsable ON (Alumnos.id = AlumnoResponsable.idAlumno) 
 	                                 INNER JOIN Responsables ON (Responsables.id= AlumnoResponsable.idResponsable) 
 	                                 INNER JOIN UsuarioResponsable ON (UsuarioResponsable.idResponsable=Responsables.id) 
 	                                 INNER JOIN Usuarios ON (Usuarios.id=UsuarioResponsable.idUsuario) 
-	                    WHERE Cuotas.tipo='matricula' and Alumnos.eliminado=0 and Cuotas.anio = YEAR(CURRENT_DATE()) and Usuarios.username=:user LIMIT :cantidad OFFSET :offset");
-$query2->bindValue(':cantidad', $sss, PDO::PARAM_INT);
-$query2->bindValue(':offset', $offset, PDO::PARAM_INT);
+	                    WHERE Cuotas.tipo='matricula' and Alumnos.eliminado=0 and Cuotas.anio = YEAR(CURRENT_DATE()) and Usuarios.username=:user ");
 $query2->bindValue(':user', $nombreusuario);
 $query2->execute();
 $datosprepost=1;
@@ -57,9 +55,7 @@ $query2=$cn->prepare("SELECT * FROM Cuotas
 	                                 INNER JOIN Responsables ON (Responsables.id= AlumnoResponsable.idResponsable) 
 	                                 INNER JOIN UsuarioResponsable ON (UsuarioResponsable.idResponsable=Responsables.id) 
 	                                 INNER JOIN Usuarios ON (Usuarios.id=UsuarioResponsable.idUsuario) 
-                                     WHERE Cuotas.tipo='matricula' and Alumnos.eliminado=0 and Cuotas.anio = YEAR(CURRENT_DATE()) and Usuarios.username=:user ORDER BY Cuotas.fechaAlta  LIMIT :cantidad OFFSET :offset");
-$query2->bindValue(':cantidad', $sss, PDO::PARAM_INT);
-$query2->bindValue(':offset', $offset, PDO::PARAM_INT);
+                                     WHERE Cuotas.tipo='matricula' and Alumnos.eliminado=0 and Cuotas.anio = YEAR(CURRENT_DATE()) and Usuarios.username=:user ORDER BY Cuotas.fechaAlta ");
 $query2->bindValue(':user', $nombreusuario);
 $query2->execute();
 $datosprepost=2;
@@ -84,9 +80,7 @@ $query2=$cn->prepare("SELECT * FROM (SELECT * FROM Cuotas WHERE Cuotas.mes < MON
  FROM Usuarios  INNER JOIN UsuarioResponsable ON (Usuarios.id = UsuarioResponsable.idUsuario) 
                 INNER JOIN AlumnoResponsable  ON ( UsuarioResponsable.idResponsable = AlumnoResponsable.idResponsable)
                 INNER JOIN Alumnos ON (Alumnos.id = AlumnoResponsable.idAlumno) 
-                WHERE Usuarios.username=:user) as tat2 WHERE NOT EXISTS (SELECT * FROM Pagos where Pagos.idCuota=ta1.id and Pagos.idAlumno= tat2.idAlumno) ORDER BY ta1.anio DESC,ta1.mes DESC LIMIT :cantidad OFFSET :offset");
-$query2->bindValue(':cantidad', $sss, PDO::PARAM_INT);
-$query2->bindValue(':offset', $offset, PDO::PARAM_INT);
+                WHERE Usuarios.username=:user) as tat2 WHERE NOT EXISTS (SELECT * FROM Pagos where Pagos.idCuota=ta1.id and Pagos.idAlumno= tat2.idAlumno) ORDER BY ta1.anio DESC,ta1.mes DESC");
 $query2->bindValue(':user', $nombreusuario);
 $query2->execute();
 $datosprepost=3;
