0a3151c209d2ae901a8b6c245bd12ab044ae7db4
Agustin De Luca <agustin_deluca@hotmail.com>
2015-11-25 23:40:45 +0000

    .

--- modelo/cargardatos.php
+++ modelo/cargardatos.php
@@ -1,7 +1,7 @@
 <?php
 $db_host="localhost";//:3306";
 $db_user="grupo_10";
-$db_pass="";
+$db_pass="DCerbxcYhyEA9X4T";
 $db_base="grupo_10"; 
 	$tt = new PDO("mysql:dbname=$db_base;host=$db_host",$db_user,$db_pass);
 
