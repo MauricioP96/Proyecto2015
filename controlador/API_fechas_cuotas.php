bfc4938f5320a537066134d46bc83a4022d0a2c0
mauriciop_96 <mauriciop_96@live.com>
2015-11-25 21:06:14 -0300

    comenzando APIS

---
+++ controlador/API_fechas_cuotas.php
@@ -0,0 +1,12 @@
+<?php
+require_once('../modelo/consulta_cuotas_pagas_de_alumno.php');
+$origin=$_SERVER['HTTP_ORIGIN'];
+header("Access-Control-Allow-Origin:$origin");
+if(!empty($_REQUEST['dni'])&&(!empty($_REQUEST['anio']))){
+	$cuotas_pagas=consultar_cuotas_pagas_de_alumno($_REQUEST['dni'],$_REQUEST['anio']);
+	echo json_encode($cuotas_pagas);
+	//echo '[{"fecha":"2015-11-17","numero":"123"},{"fecha":"2015-11-03","numero":"22"},{"fecha":"2015-11-03","numero":"345"}]';
+}
+else{
+	echo 'no envie parametros';
+}
---
+++ modelo/consulta_cuotas_pagas_de_alumno.php
@@ -0,0 +1,24 @@
+<?php 
+require ('../modelo/coneccionBD.php');
+function consultar_cuotas_pagas_de_alumno($dni,$anio){
+	conectarBD($cn);
+	$query=$cn->prepare('SELECT Pagos.fecha,Cuotas.numero
+						 FROM Pagos INNER JOIN Alumnos ON (Alumnos.id=Pagos.idAlumno) INNER JOIN Cuotas ON (Cuotas.id=Pagos.idCuota)
+	 					WHERE (year(fecha)=?) AND (Alumnos.numeroDoc=?) AND (Cuotas.tipo="cuota")');
+	$query->execute(array($anio,$dni));
+	return $query->fetchAll(PDO::FETCH_ASSOC);
+
+
+}
+
+function consultar_cuotas_pagas_de_alumno($dni,$anio){
+	conectarBD($cn);
+	$query=$cn->prepare('SELECT Cuotas.numero
+						 FROM Cuotas
+	 					WHERE (Cuotas.anio=?) AND (Cuotas.tipo="cuota") 
+	 					AND NOT EXIST(SELECT *
+ 									FROM Pagos INNER JOIN Alumnos ON (Pagos.idAlumno=Alumnos.id)
+ 									WHERE Alumnos.numeroDoc=? AND Pagos.idCuota=Cuota.id	)');
+	$query->execute(array($anio,$dni));
+	return $query->fetchAll(PDO::FETCH_ASSOC);
+}
