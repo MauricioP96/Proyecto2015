5d98781036aec7025b31e31172685a1781837f2a
agustin de deluca <agustin_deluca@hotmail.com>
2015-11-30 20:38:50 -0300

    199

--- modelo/cargardatos2.php
+++ modelo/cargardatos2.php
@@ -3,8 +3,9 @@ require_once ('../modelo/coneccionBD.php');
 conectarBD($tt);
 
   $query = $tt->prepare("SELECT SUM(Cuotas.monto) as suma,Meses.nombre FROM Cuotas 
-  	                     INNER JOIN Pagos on (Cuotas.id=Pagos.idCuota) 
-  	                     INNER JOIN Meses on (Meses.idMes=Cuotas.mes) where Pagos.becado=0 GROUP BY Cuotas.mes order by Cuotas.mes");
+  	                     INNER JOIN Meses on (Meses.idMes=Cuotas.mes)
+  	                     Left JOIN Pagos on (Cuotas.id=Pagos.idCuota) 
+  	                      where Pagos.becado=0 GROUP BY Cuotas.mes order by Cuotas.mes");
   $query->execute(); 
   $consulta = $query->fetchAll(PDO::FETCH_ASSOC);
   print json_encode($consulta);
